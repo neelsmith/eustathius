d23847bba3d2ac843d72bf569dda844a03a2f66e
Neel Smith <dnsmith.neel@gmail.com>
2022-07-05 12:47:50 -0400

    Add view of raw data

--- pluto/gettysburg_tm.jl
+++ pluto/gettysburg_tm.jl
@@ -58,7 +58,7 @@ md"""*Number of topics*: $(@bind n confirm(Slider(2:20, default=5, show_value = 
 
 # ╔═╡ 793a5c02-28cc-43aa-a1ac-2e5ccf0b6f16
 md"""
-LDA model for **$(n) topics**, computed with **$(iters) iterations**
+Computing LDA model for **$(n) topics**, computed with up to **$(iters) iterations**
 """
 
 # ╔═╡ 836a2093-fdeb-4aea-9c8a-c1d48fd76565
@@ -72,6 +72,21 @@ md"""### Topic definitions"""
 # ╔═╡ 946877bf-adc6-484a-91a8-b3844e6fd38a
 md"""
 *Number of terms to show* $(@bind terms_n Slider(5:50, default=10, show_value = true))
+"""
+
+# ╔═╡ 591a6683-128a-4774-9be3-72d8b77ec254
+md"""
+### Exploring documents
+"""
+
+# ╔═╡ 81cb7a24-fc9e-4af4-af09-5a45eb655da2
+md"""
+URNs for first "document" in each citable group
+"""
+
+# ╔═╡ b8792a49-ef39-40e0-ad31-2cd2b914c1bc
+md"""
+*Document index* $(@bind doc_idx Slider(1:20,show_value = true))
 """
 
 # ╔═╡ 4745008a-95ab-4258-a192-91cd0cd58de1
@@ -116,6 +131,15 @@ end
 # ╔═╡ 81fbe20a-75e4-4c20-8aab-aabe20f0e224
 showtopics(model, cols = n, terms_n)
 
+# ╔═╡ 1f789e6a-411b-4943-8e67-6661799ae167
+model.beta
+
+# ╔═╡ 7d7cbbce-da6b-4f78-9ba9-41ebcbc16efb
+topicdist(model, doc_idx)
+
+# ╔═╡ ac9f80bc-63dc-4982-a292-02afa28613ec
+showtitles(tmc,[1,5, 9, 13, 17])
+
 # ╔═╡ Cell order:
 # ╟─d6b5324e-fc7b-11ec-31b9-49b421a1a9b9
 # ╟─550e7828-af29-471a-ac08-bb886c3cafbb
@@ -125,8 +149,14 @@ showtopics(model, cols = n, terms_n)
 # ╟─836a2093-fdeb-4aea-9c8a-c1d48fd76565
 # ╟─b8039ca9-8078-44fc-8ef7-932a55c036f8
 # ╟─ae15ed3b-e72a-445c-aba8-81c73ca15952
-# ╟─946877bf-adc6-484a-91a8-b3844e6fd38a
+# ╠═946877bf-adc6-484a-91a8-b3844e6fd38a
 # ╟─81fbe20a-75e4-4c20-8aab-aabe20f0e224
+# ╟─1f789e6a-411b-4943-8e67-6661799ae167
+# ╟─591a6683-128a-4774-9be3-72d8b77ec254
+# ╟─81cb7a24-fc9e-4af4-af09-5a45eb655da2
+# ╟─ac9f80bc-63dc-4982-a292-02afa28613ec
+# ╟─b8792a49-ef39-40e0-ad31-2cd2b914c1bc
+# ╟─7d7cbbce-da6b-4f78-9ba9-41ebcbc16efb
 # ╟─4745008a-95ab-4258-a192-91cd0cd58de1
 # ╟─e3ee28c4-61e6-4cf2-8b1b-73eb0085a674
 # ╟─df489afb-b545-45f2-81c8-90b494ca4e5e
